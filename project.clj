ede1206d2379987574a76a0283b910c32c91f5f6
lbt05 <lbt0506@gmail.com>
2016-09-06 18:32:06 +0800

    update for dependency version

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.eclipse.jetty/jetty-server "9.2.10.v20150310"]
-                 [ring/ring-servlet "1.4.0"]
-                 [org.eclipse.jetty/jetty-servlet "9.2.10.v20150310"]
-                 [org.eclipse.jetty/jetty-jmx "9.2.10.v20150310"]
+                 [org.eclipse.jetty/jetty-server "9.2.17.v20160517"]
+                 [ring/ring-servlet "1.5.0-RC1"]
+                 [org.eclipse.jetty/jetty-servlet "9.2.17.v20160517"]
+                 [org.eclipse.jetty/jetty-jmx "9.2.17.v20160517"]
                  [hystrix-event-stream-clj "0.1.3"
                   :exclusions [com.netflix.hystrix/hystrix-clj]]
                  [com.netflix.hystrix/hystrix-clj "1.4.11"]])
